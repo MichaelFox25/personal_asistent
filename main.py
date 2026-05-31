c3378eb6ce7fc567663b803c6930236500fa2ca5
Michael <mihail.sedjurov@yandex.com>
2026-05-31 18:18:07 +0300

    небольшие правки

--- main.py
+++ main.py
@@ -37,10 +37,9 @@ class MyPersonalAssistantApp(QtWidgets.QMainWindow, ui_untitled.Ui_PersonalAssis
         self.voice_button_state = False
 
     def connect_input_actions(self):
-        """Подключает сигналы для ввода текста и кнопок."""
+        """Подключает сигналы для кнопок."""
         self.pushButton_4.clicked.connect(self.add_text_to_scroll_area)
         self.toolButton_9.clicked.connect(self.toggle_voice_input)
-
 
     def adjust_input_height(self):
         """
@@ -65,7 +64,6 @@ class MyPersonalAssistantApp(QtWidgets.QMainWindow, ui_untitled.Ui_PersonalAssis
         else:
             if self.textEdit_input.verticalScrollBarPolicy() != QtCore.Qt.ScrollBarAlwaysOff:
                 self.textEdit_input.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-
         self.textEdit_input.setFixedHeight(new_height)
         self.adjust_send_button_position()
 
@@ -110,12 +108,10 @@ class MyPersonalAssistantApp(QtWidgets.QMainWindow, ui_untitled.Ui_PersonalAssis
         self.ttsEngine.setProperty('rate', 180)
         voices = self.ttsEngine.getProperty('voices')
         ru_voice_id = None
-
         for voice in voices:
             if 'ru' in str(voice.languages).lower() or 'russian' in voice.name.lower():
                 ru_voice_id = voice.id
                 break
-
         if ru_voice_id is not None:
             self.ttsEngine.setProperty('voice', ru_voice_id)
         else:
@@ -147,7 +143,6 @@ class MyPersonalAssistantApp(QtWidgets.QMainWindow, ui_untitled.Ui_PersonalAssis
         """
         self.voice_button_state = not self.voice_button_state
         self.toggle_voice_button_border(self.voice_button_state)
-
         if self.voice_button_state:
             message = "Голосовой ввод активирован. Чем могу помочь?"
             self.add_message_to_chat(message, "bot")
@@ -163,35 +158,27 @@ class MyPersonalAssistantApp(QtWidgets.QMainWindow, ui_untitled.Ui_PersonalAssis
         """
         while self.voice_button_state:
             recognized_data = ""
+            error_message = "Простите, не совсем поняла вас, повторите, пожалуйста"
             try:
                 with self.microphone as source:
                     self.recognizer.adjust_for_ambient_noise(source, duration=1)
                     audio = self.recognizer.listen(source, timeout=5, phrase_time_limit=10)
             except speech_recognition.WaitTimeoutError:
-                if self.voice_button_state:
-                    continue
-                else:
-                    self.add_message_to_chat("Время ожидания записи истекло.", "bot")
-                    self.scroll_to_bottom()
-                    break
+                self.add_message_to_chat(error_message, "bot")
+                self.scroll_to_bottom()
+                continue
             except Exception as e:
                 print(f"Ошибка при записи аудио: {e}")
                 self.add_message_to_chat("Произошла ошибка при записи аудио.", "bot")
                 self.scroll_to_bottom()
                 break
-
             try:
                 if audio:
-                    self.add_message_to_chat("Распознаю...", "bot")
-                    self.scroll_to_bottom()
                     recognized_data = self.recognizer.recognize_google(audio, language="ru").lower()
-                    self.add_message_to_chat(f"Вы сказали: {recognized_data}", "user")
                     self.process_user_request(recognized_data)
             except speech_recognition.UnknownValueError:
-                error_message = "Простите, не совсем поняла вас, повторите, пожалуйста"
                 self.add_message_to_chat(error_message, "bot")
-                if self.voice_button_state:
-                    self.play_voice_assistant_speech(error_message)
+                self.play_voice_assistant_speech(error_message)
                 self.scroll_to_bottom()
             except Exception as e:
                 print(f"Ошибка при распознавании речи: {e}")
@@ -222,18 +209,16 @@ class MyPersonalAssistantApp(QtWidgets.QMainWindow, ui_untitled.Ui_PersonalAssis
         Если запрос приводит к открытию сайта, голосовой ввод отключается.
         """
         command = request.lower()
-
         if command in ["привет", "здравствуйте", "добрый день"]:
             bot_response = "Здравствуйте! Чем могу помочь?"
             self.add_message_to_chat(bot_response, "bot")
             return
-
         found_link = False
         for key, value in self.responses.items():
             if key in command:
                 try:
                     webbrowser.open(value, new=2, autoraise=True)
-                    response_message = "Нашел информацию. Открываю "
+                    response_message = "Нашел информацию. Открываю"
                     self.add_message_to_chat(response_message, "bot")
                     found_link = True
                     self.voice_button_state = False
@@ -278,7 +263,6 @@ if __name__ == "__main__":
     recognizer = speech_recognition.Recognizer()
     microphone = speech_recognition.Microphone()
     ttsEngine = pyttsx3.init()
-
     exit_code = app.exec_()
     if 'ttsEngine' in locals() and hasattr(ttsEngine, 'stop'):
         ttsEngine.stop()
